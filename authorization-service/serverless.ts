1d61f6ad101027b1cc304a8d08e6711e22d01bb8
Viachaslau Shpileuski <viachaslau.shpileuski@coxautoinc.com>
2022-11-14 23:09:48 +0200

    feat: complete task 7

---
+++ authorization-service/serverless.ts
@@ -0,0 +1,45 @@
+import type { AWS } from '@serverless/typescript';
+
+import basicAuthorizer from '@functions/basicAuthorizer';
+
+const serverlessConfiguration: AWS = {
+  service: 'authorization-service',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild', 'serverless-offline'],
+  useDotenv: true,
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    stage: 'dev',
+    region: 'eu-west-1',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      TestUserName: '${env:TestUserName}',
+      SlavaJSFE: '${env:SlavaJSFE}'
+    },
+  },
+  functions: { basicAuthorizer },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+    'serverless-offline': {
+      httpPort: 5000,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
--- product-service/src/functions/getProductList/index.ts
+++ product-service/src/functions/getProductList/index.ts
@@ -7,8 +7,8 @@ export default {
       http: {
         method: 'get',
         path: 'products',
-        cors: true
-      }
-    }
-  ]
+        cors: true,
+      },
+    },
+  ],
 };
